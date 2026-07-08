3839414c4129c0b495b8d6ef25114005b246bc76
###### <cristiansena000@gmail.com>
2026-07-08 16:31:42 -0500

    Primer commit

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -148,3 +150,5 @@ EMAIL_HOST_PASSWORD = 'yagf vhvt iwci fvql'
 LOGIN_URL = '/login/'
 
 LOGIN_REDIRECT_URL = '/index/'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
